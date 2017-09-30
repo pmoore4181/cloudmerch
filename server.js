6606ceff3d1891028360f09952743ded3e1f1f7b
JCFlores <jflores717@gmail.com>
2017-09-30 18:30:08 -0500

    edited users_test to shopping_cart in server.js

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 
 const db = mongoose.connection;
 
-before((done) => {
-  mongoose.connect('mongodb://localhost/shopping_cart', {
-    useMongoClient: true,
-  });
-
-  db
-    .on('error', (error) => {
-      console.warn('Warning', error);
-    })
-    .once('open', () => {
-      console.log('Connected to shopping_cart database!');
-      done();
-    });
+// before((done) => {
+mongoose.connect('mongodb://localhost/shopping_cart', {
+  useMongoClient: true,
 });
+
+db
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  })
+  .once('open', () => {
+    console.log('Connected to shopping_cart database!');
+    // done();
+  });
+// });
