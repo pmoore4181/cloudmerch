a87fa59ef505875dfc2a4fbeb22eac553d878245
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-10-07 01:19:43 -0500

    updated find store route

--- controllers/routes.js
+++ controllers/routes.js
@@ -4,6 +4,10 @@ var Store = require("../database/models/store");
 var express = require('express');
 var router = express.Router();
 
+////////////////
+// GET ROUTES //
+////////////////
+
 router.get("/", function(req, res) {
     res.send("yahoo");
 })
@@ -38,31 +42,65 @@ router.get("/sellers/:sellername", function(req, res) {
     })
 });
 
-// FIND A SPECIFIC STORE FROM SPECIFIC SELLER - working
-router.get("/sellers/:sellername/:storeid", function(req, res) {
+// FIND A SPECIFIC STORE FROM SPECIFIC SELLERv1 - working
+
+// router.get("/sellers/:sellername/:storeid", function(req, res) {
+
+//     var sellername = req.params.sellername;
+//     var storeid = req.params.storeid;
+
+//     console.log(`${sellername}'s store, ${storeid}, accessed`);
+
+//     Seller.find({ 'name': sellername })
+//         .exec(function(err, doc) {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             if (doc[0].store[0]._id == storeid) {
+//                 res.json(doc[0].store[0]);
+//             }
+//             if (doc[0].store[1]._id == storeid) {
+//                 res.json(doc[0].store[1]);
+//             }
+//         })
+// });
+
+// FIND A SPECIFIC STORE FROM SPECIFIC SELLERv2 - working
+router.get("/sellers/:sellername/:storename", function(req, res) {
 
     var sellername = req.params.sellername;
-    var storeid = req.params.storeid;
+    var storename = req.params.storename;
 
-    console.log(`${sellername}'s store, ${storeid}, accessed`);
+    console.log(`${sellername}'s store, ${storename}, accessed`);
 
-    Seller.find({ 'name': sellername })
+    Seller.aggregate([{ $unwind: "$store" }, { $match: { "name": sellername, "store.name": storename } }, { $project: { "_id": 0, "store": 1 } }])
         .exec(function(err, doc) {
-            if (err) {
-                console.log(err);
+                if (err) {
+                    console.log(err)
+                } else {
+                    res.json(doc)
+                }
             }
-            if (doc[0].store[0]._id == storeid) {
-                res.json(doc[0].store[0]);
-            }
-            if (doc[0].store[1]._id == storeid) {
-                res.json(doc[0].store[1]);
-            }
-        })
+
+        );
+})
+
+// FIND PRODUCTS FROM A SPECIFICS STORE - not yet working
+// DOES IT NEED TO WORK?
+router.get("/sellers/:sellername/:storename/products", function(req, res) {
+
+    var sellername = req.params.sellername;
+    var storename = req.params.storename;
+
+    console.log(`${sellername}'s store, ${storename} products, accessed`);
+
 });
 
-//
-// POST ROUTES
-//
+
+
+/////////////////
+// POST ROUTES //
+/////////////////
 
 //  ADD NEW SELLER -working
 router.post("/sellers", function(req, res) {
@@ -82,7 +120,7 @@ router.post("/sellers/:sellername/stores", function(req, res) {
 
     var body = req.body;
 
-    Seller.findOneAndUpdate({ "name": req.params.sellername }, { $push: { store: body}},{ new: true },
+    Seller.findOneAndUpdate({ "name": req.params.sellername }, { $push: { store: body } }, { new: true },
         function(err, numAffected) {
             if (err) {
                 console.log(err)
@@ -92,24 +130,40 @@ router.post("/sellers/:sellername/stores", function(req, res) {
         });
 });
 
-//ADD NEW PRODUCTS TO STORE
+//ADD NEW PRODUCTS TO STORE - not yet working
 router.post("/sellers/:sellername/:storename/products", function(req, res) {
 
+    var sellername = req.params.sellername;
+    var storename = req.params.storename;
     var body = req.body;
-
-    
 
 
 });
 
+////////////////
+// PUT ROUTES //
+////////////////
 
-
-// PUT ROUTES
+// UPDATE SELLER INFO
 router.put("/sellers", function(req, res) {
 
 });
 
-// DELETE ROUTES
+// UPDATE SPECIFIC STORE INFO
+
+// UPDATE SPECIFIC PRODUCT INFO
+
+
+///////////////////
+// DELETE ROUTES //
+///////////////////
+
+// DELETE PRODUCT
+
+// DELETE STORE
+
+// DELETE SELLER
+
 router.delete("/sellers", function(req, res) {
 
 });
