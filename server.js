dcfd00d7b303255853ab94ee67bd90186827e120
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-10-08 18:24:37 -0500

    herokue pushhhhh

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ require('./services/passport');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+// }
 
 // Run Morgan and BodyParser
 app.use(logger('dev'));
