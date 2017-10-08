003f71d13e4eb1ce61b0d7139dd199cc8ccfdcbf
JCFlores <jflores717@gmail.com>
2017-10-08 15:56:17 -0500

    set up production and development keys for heroku deployment

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// Heroku sets NODE_ENV variable
+if (process.env.NODE_ENV === 'production') {
+  // we are in production - return the prod set of keys
+  module.exports = require('./prod');
+} else {
+  // we are in development = return the dev keys
+  module.exports = require('./dev');
+}
