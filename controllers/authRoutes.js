1c690698b52064359db366dfd36ec90958fa977a
JCFlores <jflores717@gmail.com>
2017-10-12 00:15:00 -0500

    user is prompted to select account with google sign up, and redirected to sellers home page

--- controllers/authRoutes.js
+++ controllers/authRoutes.js
@@ -4,9 +4,16 @@ const passport = require('passport');
 module.exports = (app) => {
   app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email'],
+    prompt: 'select_account',
   }));
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/user-login');
+    },
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
